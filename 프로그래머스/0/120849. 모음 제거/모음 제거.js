a57949b74149160cc0c1ddf083e737f4e56ac2e2
accguy <104961021+accguy@users.noreply.github.com>
2024-07-15 18:29:44 +0900

    [level 0] Title: 모음 제거, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,11 +1,3 @@
       function solution(my_string) {
-        var answer = "";
-        answer = my_string.replaceAll("a", "");
-        answer = answer.replaceAll("e", "");
-        answer = answer.replaceAll("i", "");
-        answer = answer.replaceAll("o", "");
-        answer = answer.replaceAll("u", "");
-        return answer;
-
-        // return my_string.replace(/[aeiou]/g, ''); // replace + 정규표현식
+        return my_string.replace(/[aeiou]/g, "")
       }
