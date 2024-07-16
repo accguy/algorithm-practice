bac2b7739c92fb9b8ff67e3fb8cee36b6d485841
accguy <104961021+accguy@users.noreply.github.com>
2024-07-16 15:26:02 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,6 @@
 function solution(my_string) {
-    return my_string.split("").filter(item => !isNaN(Number(item))).reduce((acc,cur)=>acc+Number(cur),0);
+    return my_string
+        .split("")
+        .filter(item => !isNaN(Number(item)))
+        .reduce((acc,cur) => acc + Number(cur), 0);
 }
