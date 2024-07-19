d62d7b31749f70fb713773992604b1f8b6eb9283
accguy <104961021+accguy@users.noreply.github.com>
2024-07-19 16:50:33 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,4 @@
 function solution(n) {
-    n.sort((a,b) => a-b);n.at(-1)*n.at(-2);
-    return n[0]*n[1] > n.at(-1)*n.at(-2) ?  n[0]*n[1] : n.at(-1)*n.at(-2);
+    n.sort((a,b) => a-b);
+    return Math.max(n[0]*n[1], n.at(-1)*n.at(-2));
 }
