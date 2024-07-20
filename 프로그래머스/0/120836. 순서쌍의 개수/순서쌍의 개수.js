467060415652458ee803f695526a366a23b4df6a
accguy <104961021+accguy@users.noreply.github.com>
2024-07-21 03:31:21 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for (let i = 1; i <= Math.sqrt(n); i++){
+        if(n % i == 0) {
+            answer++
+         }
+    }    
+    return Number.isInteger(Math.sqrt(n)) ? answer*2 - 1 : answer*2;
+
+}
