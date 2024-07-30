3917ea24c84661c05bbf9f840ed44c103b0fd9e5
accguy <104961021+accguy@users.noreply.github.com>
2024-07-30 09:23:42 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.replaceAll(/[a-zA-Z]/g, '-').split('-').map(x=>Number(x)).reduce((acc,cur)=>acc+=cur);
+    return my_string.replace(/[a-zA-Z]/g, '-').split('-').map(x=>Number(x)).reduce((acc,cur)=>acc+=cur);
 }
