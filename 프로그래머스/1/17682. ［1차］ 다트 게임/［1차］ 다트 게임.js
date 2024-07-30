c4101288dc3db43c79f4bd6e9e1243e093141375
accguy <104961021+accguy@users.noreply.github.com>
2024-07-30 16:55:06 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.51 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,22 @@
+function solution(dartResult) {
+    var array = dartResult.match(/\d+[SDT][*#]?/g);
+    var answer = array.map(v => parseInt(v.match(/\d+/)));
+    // 제곱, 세제곱 계산하기 
+    for (let i = 0; i < 3; i++){
+        if (array[i].match(/[SDT]/)[0] == "D") {
+            answer[i] **= 2;
+        } else if (array[i].match(/[SDT]/) == "T") {
+            answer[i] **= 3;
+        }
+    }
+    // *, # 계산하기
+    for (let i = 0; i < 3; i++) {
+        if (array[i].match(/[*#]/) == "*") {
+            answer[i] *= 2;
+            answer[i-1] *= 2;
+        } else if (array[i].match(/[*#]/) == "#"){
+            answer[i] *= -1;
+        }
+    }
+    return answer.reduce((a, c) => a + c);
+}
