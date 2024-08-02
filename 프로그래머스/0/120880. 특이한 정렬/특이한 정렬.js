60c540f4849903e41a1e8e34b83d0a1bbe76d06f
accguy <104961021+accguy@users.noreply.github.com>
2024-08-02 11:25:12 +0900

    [level 0] Title: 특이한 정렬, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -3,16 +3,12 @@ function solution(numlist, n) {
   const m = new Map();
 
   for (const x of numlist) { m.set(x, Math.abs(n - x)); }
-  const array = [...m].sort((a, b) => a[1] - b[1]);
-
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i][1] == array[i+1][1] && array[i][0] < array[i+1][0]) {
-      answer.push(array[i+1][0]);
-      array[i+1] = array[i];
-    } else {
-      answer.push(array[i][0]);
-    }
-  }
-  answer.push(array[array.length-1][0]);
+  const array = [...m].sort((a, b) => {
+      // 차이가 같다면 값을 기준으로 정렬(내림차순)
+      if (a[1] == b[1]) { return b[0] - a[0]; } 
+      // 차이가 다르다면 차이를 기준으로 정렬(오름차순)
+      else { return a[1] - b[1]; }
+  });
+  array.map(v=>answer.push(v[0]))
   return answer;
 }
