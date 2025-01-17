fe61023fd8b9882d2efae0b53c0b2e586aa00429
accguy <104961021+accguy@users.noreply.github.com>
2025-01-17 18:16:11 +0900

    [level 3] Title: N으로 표현, Time: 80.67 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -2,12 +2,11 @@ function solution(N, number) {
     // N을 i번 사용한 숫자들의 집합 배열
     let dp = Array.from({length: 9}, () => new Set);
     
-    // {N}, {NN}, {NNN}, ... 으로 초기화 
-    for(let i = 1; i < 9; i++){
-        dp[i].add(Number(String(N).repeat(i)));
-    }
-    
     for(let i = 1; i < 9; i++) {
+        // {N}, {NN}, {NNN}, ... 으로 초기화 
+        dp[i].add(Number(String(N).repeat(i)));
+        
+        // i = j + (i-j) 
         for(let j = 1; j < i; j++) {
             for(let x of dp[j]) {
                 for(let y of dp[i-j]) {
