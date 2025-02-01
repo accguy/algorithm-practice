955ec5eba402de0bc8ac75e235ce79a61eba960f
accguy <104961021+accguy@users.noreply.github.com>
2025-02-01 12:53:06 +0900

    [Silver V] Title: 색종이, Time: 96 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const N = Number(input[0]);
+const bg = Array.from({ length: 100 }, () => Array(100).fill(0));
+for (let i = 1; i <= N; i++){
+    const [x, y] = input[i].split(" ").map(Number);
+    
+    for(let j = y; j < y + 10; j++){
+        for(let k = x; k < x + 10; k++){
+            if(j<=100 && k<= 100){
+                bg[j][k] = 1;    
+            }
+        }
+    }
+}
+console.log(bg.flat().reduce((a, c) => a + c, 0));
