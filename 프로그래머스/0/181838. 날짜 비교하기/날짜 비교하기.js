72264b14c79ae110f9bae63ead6f6c2ed3d7170b
accguy <104961021+accguy@users.noreply.github.com>
2025-02-12 16:43:52 +0900

    [level 0] Title: 날짜 비교하기, Time: 7.05 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,7 +1,4 @@
 function solution(date1, date2) {
-    const d = date1.map((x,i)=>x-date2[i]);
-    if(d[0]<0){return 1}
-    if(d[0]==0&&d[1]<0){return 1}
-    if(d[0]==0&&d[1]==0&&d[2]<0){return 1}
-    return 0;
+    console.log(new Date(date1));
+    return new Date(date1) < new Date(date2) ? 1 : 0;
 }
