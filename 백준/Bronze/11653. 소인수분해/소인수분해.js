056b180637a8a0026601f2aa2789e9f5619548ed
accguy <104961021+accguy@users.noreply.github.com>
2025-02-18 19:04:22 +0900

    [Bronze I] Title: 소인수분해, Time: 124 ms, Memory: 9628 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -1,6 +1,6 @@
 let N = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
 
-for(let i = 2; i <= Math.sqrt(N); i++) {
+for(let i = 2; i*i <= N; i++) { // Math.sqrt()는 처음에 한번만 계산됨
     while(N % i === 0) {
         console.log(i)
         N /= i;
