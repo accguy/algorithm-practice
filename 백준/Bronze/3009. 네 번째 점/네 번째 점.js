324cb58adfbafc8083b920140dbead796afa6f9d
accguy <104961021+accguy@users.noreply.github.com>
2025-02-19 18:02:33 +0900

    [Bronze III] Title: 네 번째 점, Time: 88 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.js
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.js
@@ -1,8 +1,19 @@
 const arr = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n").map(x => x.split(" ").map(Number));
-let x = null;
-let y = null;
-arr.sort((a,b)=>a[0]-b[0]);
-x = arr[0][0] == arr[1][0] ? arr[2][0] : arr[0][0];
-arr.sort((a,b)=>a[1]-b[1]);
-y = arr[0][1] == arr[1][1] ? arr[2][1] : arr[0][1];
+
+let x, y;
+const xCount = {};
+const yCount = {};
+
+for(let [a, b] of arr) {
+    xCount[a] = (xCount[a] || 0) + 1;
+    yCount[b] = (yCount[b] || 0) + 1;
+}
+
+for(const key in xCount) {
+    if(xCount[key] == 1) x = key;
+}
+for(const key in yCount) {
+    if(yCount[key] == 1) y = key;
+}
+
 console.log(`${x} ${y}`);
