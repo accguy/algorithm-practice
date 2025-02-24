383819b30b2bc45738105ba59bd1c2ad2997cef2
accguy <104961021+accguy@users.noreply.github.com>
2025-02-24 23:33:55 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.48 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,8 +1,13 @@
 function solution(arr, queries) {
     const answer = [];
     for(let [s,e,k] of queries) {
-        const subArr = arr.slice(s,e+1).filter(x => x>k);
-        answer.push(subArr.length ? Math.min(...subArr) : -1)
+        let minValue = Infinity;
+        for(let i = s; i <= e; i++){
+            if(arr[i] > k && arr[i] < minValue) {
+                minValue = arr[i];
+            }
+        }
+        answer.push(minValue !== Infinity ? minValue : -1);
     }
     return answer;
 }
