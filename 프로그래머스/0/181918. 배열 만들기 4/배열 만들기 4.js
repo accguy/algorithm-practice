5e36ab18adc45b679a4b37313e8c6659c3066b99
accguy <104961021+accguy@users.noreply.github.com>
2025-02-26 17:38:57 +0900

    [level 0] Title: 배열 만들기 4, Time: 8.29 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -4,8 +4,7 @@ function solution(arr) {
     
     while(i < arr.length) {
             if(stk[stk.length-1] < arr[i] || stk.length === 0) {
-                stk.push(arr[i]);
-                i++;
+                stk.push(arr[i++]);
             } else {
                 stk.pop();
             }
