102a1c40a19549cb92bde3f12bda652d8b9fe042
accguy <104961021+accguy@users.noreply.github.com>
2025-03-01 19:11:30 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,20 +1,14 @@
 function solution(s) {
-    const data = {};
-    // A~Z: 65~90 
-    // (91~96) -> (26~31)
-    // a~z: 97~122 
+    const charCount = Array(52).fill(0);
+    // A~Z: 65~90 -> 0~25
+    // a~z: 97~122 -> 26~51
     for(let i = 0; i < s.length; i++) {
         const code = s[i].charCodeAt();
-        data[code] = (data[code] || 0) + 1;
+        if(code <= 90) {
+            charCount[code-65] = (charCount[code-65] || 0) + 1;
+        } else {
+            charCount[code-71] = (charCount[code-71] || 0) + 1;
+        }
     }
-    
-    let list = Array(123).fill(0); // 0~122
-    for(const key in data){
-        list[key] = data[key];
-    }
-    
-    list = list.slice(65);
-    const A = list.slice(0,26);
-    const B = list.slice(32);
-    return A.concat(B);
+    return charCount;
 }
