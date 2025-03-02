a2ab6f27bbeabb5d1855fa51b1053aa8d41901b3
accguy <104961021+accguy@users.noreply.github.com>
2025-03-02 18:09:55 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,8 +1,8 @@
 function solution(a, b, c, d) {
-    var answer = 0;
     const arr = [a,b,c,d];
     const size = new Set(arr).size;
     const count = Array(7).fill(0); // 1~6의 개수를 저장할 배열
+    
     for(let i = 0; i < arr.length; i++){
         const cur = arr[i];
         count[cur]++;
@@ -10,31 +10,29 @@ function solution(a, b, c, d) {
 
     switch (size) {
         case 1: 
-            answer = 1111 * a;
-            break;
+            return 1111 * a;
         case 2:
-            if(count.indexOf(2)==-1){
-                // 3 + 1
-                const p = count.indexOf(3);
+        {
+            const p = count.indexOf(3);
+            if(p != -1) {
+                // 3+1 의 경우
                 const q = count.indexOf(1);
-                answer = (10 * p + q)**2 
-                break;
+                return (10 * p + q)**2;         
             } else {
-                // 2 + 2
-                arr.sort();
-                const p = arr[0];
-                const q = arr[3];
-                answer = (p + q) * Math.abs(p - q);
-                break;  
+                // 2+2 의 경우
+                // 블록 내의 새로운 블록이기때문에 const 재선언 가능
+                const [p,q] = count.map((x,i)=>x? i:null).filter(i=>i); 
+                return (p + q) * Math.abs(p - q);            
             }
-        case 3:
-            const p = count.indexOf(1);
-            const q = count.lastIndexOf(1);
-            answer =  p * q;
-            break;
+        }
+        case 3: 
+        {
+            const [p,q] = count.map((x,i)=>x==1?i:null).filter(i=>i);
+            return p * q;     
+        }      
         case 4:
-            answer = Math.min(...arr);
-            break;       
+            return Math.min(...arr);                
     }
-    return answer;
 }
+
+// 
