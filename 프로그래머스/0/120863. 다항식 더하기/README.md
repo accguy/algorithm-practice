5a6e0c31595b69958f6ea00cdf7b1395fa8df4ff
accguy <104961021+accguy@users.noreply.github.com>
2025-03-03 19:44:31 +0900

    [level 0] Title: 다항식 더하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/README.md
+++ 프로그래머스/0/120863. 다항식 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다항식 더하기 - 120863 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.18 ms
+메모리: 33.4 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 19:33:07
+2025년 03월 03일 19:44:29
 
 ### 문제 설명
 
--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,12 +1,19 @@
 function solution(p) {
     const arr = p.split(" + ");
-    const xArr = arr.filter(x => x[x.length-1]=='x').map(x => x=='x' ? 1 : Number(x.slice(0, x.length-1)));
-    const cArr = arr.filter(x => x[x.length-1]!='x').map(Number);
+    const xArr = arr.filter(x => x[x.length-1]=='x')
+                .map(x => x=='x' ? 1 : Number(x.slice(0, x.length-1)));
+    const cArr = arr.filter(x => x[x.length-1]!='x')
+                .map(Number);
     
     // console.log(xArr, cArr);
     const A = xArr.length ? xArr.reduce((a,c)=>a+c, 0) : 0;
     const B = cArr.length ? cArr.reduce((a,c)=>a+c, 0) : 0;
-    return A && B ? A == 1 ? `x + ${B}` : `${A}x + ${B}` : !A ? `${B}` : A == 1 ? `x` : `${A}x`;
+    
+    let answer = [];
+    if(A) answer.push(A == 1 ? `x` : `${A}x`);
+    if(B) answer.push(`${B}`);
+    
+    return answer.join(" + "); // answer의 요소가 1개일때는 해당 문자열을 반환합니다.
 }
 
 // A = 1 일때 'x'로 바꿔주기 
