5165282a5d32165aceffe1593d523b43bdac953c
accguy <104961021+accguy@users.noreply.github.com>
2025-03-03 19:48:49 +0900

    [level 0] Title: 다항식 더하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,19 +1,16 @@
 function solution(p) {
     const arr = p.split(" + ");
-    const xArr = arr.filter(x => x[x.length-1]=='x')
-                .map(x => x=='x' ? 1 : Number(x.slice(0, x.length-1)));
-    const cArr = arr.filter(x => x[x.length-1]!='x')
+    const xArr = arr.filter(x => x.includes('x'))
+                .map(x => x=='x' ? 1 : Number(x.replace('x','')));
+    const cArr = arr.filter(x => !x.includes('x'))
                 .map(Number);
     
-    // console.log(xArr, cArr);
     const A = xArr.length ? xArr.reduce((a,c)=>a+c, 0) : 0;
     const B = cArr.length ? cArr.reduce((a,c)=>a+c, 0) : 0;
     
     let answer = [];
-    if(A) answer.push(A == 1 ? `x` : `${A}x`);
+    if(A) answer.push(A == 1 ? `x` : `${A}x`); // A = 1 일때 'x'로 바꿔주기 
     if(B) answer.push(`${B}`);
     
     return answer.join(" + "); // answer의 요소가 1개일때는 해당 문자열을 반환합니다.
 }
-
-// A = 1 일때 'x'로 바꿔주기 
