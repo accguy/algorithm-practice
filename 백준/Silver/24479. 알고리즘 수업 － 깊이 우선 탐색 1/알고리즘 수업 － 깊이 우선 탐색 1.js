11750d247df714fdbf850cbd72c193f2ff29b083
accguy <104961021+accguy@users.noreply.github.com>
2025-05-04 16:03:53 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 3992 ms, Memory: 88036 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
@@ -1,28 +1,26 @@
-const input = require('fs').readFileSync(0, 'utf-8').toString().trim().split("\n");
-const [N, M, R] = input[0].split(" ").map(Number); // 정점, 간선, 시작노드
+const input = require('fs').readFileSync(0,'utf-8').toString().trim().split("\n");
+const [N, M, R] = input[0].split(" ").map(Number);
 
-const g = Array.from({length: N + 1}, () => []); // 그래프
+const g = Array.from({length: N+1}, ()=>[]);
 for(let i = 1; i <= M; i++) {
-    const [a, b] = input[i].split(" ").map(Number);
+    const [a,b] = input[i].split(" ").map(Number);
     g[a].push(b);
     g[b].push(a);
 }
-g.forEach(li => li.sort((a,b) => a-b));
+g.forEach((li) => li.sort((a,b)=>a-b));
 
 const visited = Array(N+1).fill(0);
 let order = 0;
 
-
 const dfs = (node) => {
     visited[node] = ++order;
     for(const x of g[node]){
-        if(!visited[x]){
-            dfs(x);
-        }
+        if(!visited[x]) dfs(x);
     }
 }
 
 dfs(R);
+
 for(let i = 1; i <= N; i++){
     console.log(visited[i]);
 }
